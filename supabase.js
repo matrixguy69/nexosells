edf500dfd58cc4f71b32cac7b063f8082435d6c1
matrixguy69 <blouchsaab587@gmail.com>
2026-05-02 13:53:12 +0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -10,7 +10,7 @@
 // ═══════════════════════════════════════════════════════════════
 
 const SUPABASE_URL = 'https://yapshqzfknpssoiklbcr.supabase.co';    // ← paste here
-const SUPABASE_ANON_KEY = 'sb_publishable_ZFJLT1q0QehsF9IyhgCWmA_D3SP-nvy';                // ← paste here
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhcHNocXpma25wc3NvaWtsYmNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NjU4ODksImV4cCI6MjA5MzI0MTg4OX0.NJqEwVGKCEUiRwdvByugulCd5IQ3vC40FuyiU2MjCH0';                // ← paste here
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
